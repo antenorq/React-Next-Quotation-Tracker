bc9d92f296b9ea7977597cc5ce96abd50a292585
antenorq <antenorq@gmail.com>
2024-01-18 00:01:51 -0500

    updated mongdb access config

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,7 @@ const dbPassword = encodeURIComponent(process.env.DB_PASSWORD);
 
 const conn = async () => {
   try {
-    const dbConn = await mongoose.connect(
-      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.eilvvjp.mongodb.net/igloo?retryWrites=true&w=majority`
-    );
+    const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.stvnq4v.mongodb.net/tracker?retryWrites=true&w=majority`);
 
     console.log("Database Connected!!");
 
